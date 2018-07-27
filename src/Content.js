f9680be79501da96d2502071b4a74f7b286a54e8
Eric Humphrey <erichumphrey10@yahoo.com>
2018-07-26 21:43:48 -0500

    Change Pages directory case

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,15 @@
-import * as Pages from './Pages';
+import * as Pages from './pages';
 import Eric from '../images/Eric.JPG';
 
 export default function Content(state, posts){
     return `
-<div id="content">
-        <div class="container">
-    <p>What up world. I am a beginner, but I will get it together!</p>
-    <h3>some contact information</h3>
-    <img src="${Eric}" alt="my face"></div>
-        ${Pages[state.body](posts)}
-    </div>
-    </div>
-`;
+        <div id="content">
+            <div class="container">
+                <p>What up world. I am a beginner, but I will get it together!</p>
+                <h3>some contact information</h3>
+                <img src="${Eric}" alt="my face">
+                ${Pages[state.body](posts)}
+            </div>
+        </div>
+    `;
 }
